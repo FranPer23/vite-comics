7c2171e5e1c74a15da1e45275d6b75f08664701d
FranPer23 <francescaprt@yahoo.com>
2023-04-13 15:09:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+  import AppHeader from "./components/AppHeader.vue";
+
+
+  export default {
+    components: { AppHeader }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style scope lang="scss">
+  @use "../src/assets/style/general.scss"
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+        name: "AppHeader",
+        data(){
+
+        }
+    }
+</script>
+
+<template>
+    <h1>Questo è il mio header</h1>
+</template>
+
+<style scope lang="scss">
+
+</style>
+
